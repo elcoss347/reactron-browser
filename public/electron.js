4ded7b1b8f90e351cc130e71ab304c32ba77fb16
Elcoss <jp14700@gmail.com>
2020-05-27 04:59:46 -0300

    config electron browser

--- public/electron.js
+++ public/electron.js
@@ -1,16 +1,28 @@
 const electron = require('electron');
+const config = require('./config')
 const app = electron.app;
+const globalShortcut = electron.globalShortcut
 const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({width: 900, height: 680, alwaysOnTop:true});
-  mainWindow.loadURL('http://youtube.com.br');
+  mainWindow.loadURL(config.url);
   mainWindow.on('closed', () => mainWindow = null);
 }
 
-app.on('ready', createWindow);
+function toggleDevTools() {
+  mainWindow.webContents.toggleDevTools()
+}
+
+function createShortcut(){
+  globalShortcut.register('CmdOrCtrl+J',toggleDevTools)
+}
+
+app.whenReady()
+.then(createWindow)
+.then(createShortcut);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -21,5 +33,6 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
   if (mainWindow === null) {
     createWindow();
+    createShortcut();
   }
 });
